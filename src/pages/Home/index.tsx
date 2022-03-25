af79c4ada1f3970cb899847352d3e5f70555753e
Almir Crispiniano <37631373+almirgon@users.noreply.github.com>
2022-03-25 14:23:38 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import Loading from "../../components/Loading";
 import Card from "../../components/Card";
 import {useHistory} from 'react-router-dom'
+import api from '../../services/api'
 
 interface Item {
   name?: string;
@@ -22,60 +23,11 @@ const Home = () => {
 
   useEffect(() => {
     setLoading(true)
-    const mock = [
-      {
-        id: 1,
-        name: "Coleta de Lixo Eletronico",
-        email: "eletronico@gmail.com",
-        whatsapp: "83 99999-9999",
-        uf: "PB",
-        city: "João Pessoa",
-        latitude: -7.11532,
-        longitude: -34.861,
-      },
-      {
-        id: 2,
-        name: "Coleta de Roupas para Desabrigados",
-        email: "desabrigadosrj@gmail.com",
-        whatsapp: "12 66666-9999",
-        uf: "RJ",
-        city: "Rio de Janeiro",
-        latitude: -22.9035,
-        longitude: -43.2096,
-      },
-      {
-        id: 3,
-        name: "Coleta de Material Reciclavel",
-        email: "reclica_recife@hotmail.com",
-        whatsapp: "81 99999-7777",
-        uf: "PE",
-        city: "Recife",
-        latitude: -8.05428,
-        longitude: -34.8813,
-      },
-      {
-        id: 4,
-        name: "Coleta de Alimentos para atingidos pelas enchentes",
-        email: "petropolis@outlook.com",
-        whatsapp: "83 88888-6666",
-        uf: "RJ",
-        city: "Petropolis",
-        latitude: -22.5046,
-        longitude: -43.1823,
-      },
-      {
-        id: 5,
-        name: "Coleta de Sapatos",
-        email: "sapatoaqui@gmail.com",
-        whatsapp: "23 66655-9856",
-        uf: "BA",
-        city: "Salvador",
-        latitude: -12.9704,
-        longitude: -38.5124,
-      },
-    ];
-    setItems(mock)
-    setLoading(false)
+    api.get('/all').then(({data,status}) => {if(status === 200){
+      setItems(data.response)
+      setLoading(false)
+    }})
+    
   }, [])
 
   const handleClick = (item) => {
